5b67eead6784e139dabb0aa0b2dbce98d40aec66
PremKumar Sahebraj Mistry <120615589+premkumarmistry@users.noreply.github.com>
2024-09-22 18:50:32 +0530

    Create class.dart

---
+++ class.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Flutter App",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Material App",
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            body: Center(
+              child: SizedBox(
+                height: 200,
+                child: Card(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          height: 100,
+                          "https://cdn.pixabay.com/photo/2024/08/30/10/15/woman-9009013_1280.png",
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Yakul ki  Cutie Pie",
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )));
+  }
+}
